7767864413d2078bbcb5b8bbd467683275129521
sharmagaurav1912 <guri07cr@gmail.com>
2024-02-27 10:50:18 +0530

    add delete icon functionaity

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ btn.addEventListener("click",() => {
     let newNote = document.createElement("p");
     newNote.className = "input-box";
     newNote.setAttribute("contenteditable","true");
-
-
     container.appendChild(newNote);
 
+    let deleteIcon = document.createElement("img");
+    deleteIcon.src = "delete.png";
+    deleteIcon.className = "delete";
+    newNote.appendChild(deleteIcon);
+    
+    let deleteNote = document.querySelectorAll('.delete');
+    deleteNote.forEach((e) => {
+    
+        e.addEventListener(
+            'click',(button) => {
+            console.log('remove button clicked');
+            console.log(button.target);
+            e.parentNode.remove();
+                    
+            }
+        );
+    });
+    
 });
+
+
